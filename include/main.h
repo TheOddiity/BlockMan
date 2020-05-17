a63a92de704679692d391186c712d7c711485506
TheOddiity <theoddity@protonmail.com>
2020-05-17 23:59:59 +0200

    Moving man

--- include/main.h
+++ include/main.h
@@ -8,4 +8,14 @@ struct Resources
 
     sf::Texture playerTexture;
     sf::Sprite playerSprite;
+
+    sf::Font blackChancery;
+
+    sf::Text playerPositionText;
+};
+
+struct Player
+{
+    float posX;
+    float posY;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int windowHeight = 540;
 
 void initializeResources(Resources &res)
 {
-    if (!res.backgroundTexture.loadFromFile("assets/grass-FWStudio-Pexel.jpg"))
+    if (!res.backgroundTexture.loadFromFile("assets/grass.jpg"))
     {
         std::cerr << "Cannot load background" << std::endl;
     }
@@ -24,45 +24,54 @@ void initializeResources(Resources &res)
     res.playerSprite.setScale(playerScale, playerScale);
     res.playerSprite.setPosition((windowWidth - res.playerTexture.getSize().x * playerScale) / 2,
                                  (windowHeight - res.playerTexture.getSize().y * playerScale) / 2);
+
+    if (!res.blackChancery.loadFromFile("assets/BLKCHCRY.TTF"))
+    {
+        std::cerr << "Cannot load font" << std::endl;
+    }
+    res.playerPositionText.setFont(res.blackChancery);
+    res.playerPositionText.setPosition(10, windowHeight - 30);
+    res.playerPositionText.setCharacterSize(20);
+    res.playerPositionText.setFillColor(sf::Color::Black);
+}
+
+void handleInput(float dt, Resources &res, Player &player)
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+        player.posY -= 600 * dt;
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+        player.posY += 600 * dt;
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        player.posX -= 600 * dt;
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        player.posX += 600 * dt;
+    }
+}
+
+void update(Resources &res, Player &player)
+{
+    res.playerPositionText.setString("X: " + std::to_string(player.posX) + " Y: " + std::to_string(player.posY));
+
+    //Setting the background
+    sf::IntRect rect = res.backgroundSprite.getTextureRect();
+    rect.top = player.posY;
+    rect.left = player.posX;
+    res.backgroundSprite.setTextureRect(rect);
 }
 
 void drawScreen(sf::RenderWindow &window, Resources &res)
 {
     window.draw(res.backgroundSprite);
     window.draw(res.playerSprite);
-}
-
-void handleInput(sf::Event event, Resources &res)
-{
-    switch(event.type)
-    {
-    case sf::Event::KeyPressed:
-    if(event.key.code == sf::Keyboard::Up)
-        {
-            sf::IntRect rect = res.backgroundSprite.getTextureRect();
-            rect.top -= 10;
-            res.backgroundSprite.setTextureRect(rect);
-        }
-        else if(event.key.code == sf::Keyboard::Down)
-        {
-            sf::IntRect rect = res.backgroundSprite.getTextureRect();
-            rect.top += 10;
-            res.backgroundSprite.setTextureRect(rect);
-        }
-        else if(event.key.code == sf::Keyboard::Left)
-        {
-            sf::IntRect rect = res.backgroundSprite.getTextureRect();
-            rect.left -= 10;
-            res.backgroundSprite.setTextureRect(rect);
-        }
-        else if(event.key.code == sf::Keyboard::Right)
-        {
-            sf::IntRect rect = res.backgroundSprite.getTextureRect();
-            rect.left += 10;
-            res.backgroundSprite.setTextureRect(rect);
-        }
-        break;
-    }
+    window.draw(res.playerPositionText);
 }
 
 int main()
@@ -71,8 +80,12 @@ int main()
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Blockman is awesome!");
     window.setFramerateLimit(60);
 
+    sf::Clock frameTime;
+
     Resources res = {};
     initializeResources(res);
+
+    Player player {0, 0};
 
 	// Start the game loop
     while (window.isOpen())
@@ -84,15 +97,15 @@ int main()
             // Close window : exit
             if (event.type == sf::Event::Closed)
                 window.close();
-
-            handleInput(event, res);
         }
 
         // Clear screen
         window.clear();
 
-        // Update the window
+        handleInput(frameTime.restart().asSeconds(), res, player);
+        update(res, player);
 
+        // Update the window
         drawScreen(window, res);
         window.display();
     }
